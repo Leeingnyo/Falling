91e47f357ca65406a111bed711db0eab6b1799f5
Leeingnyo <dyd1928@naver.com>
2014-08-16 02:06:43 +0900

    스프라이트에 움직임을 부여

--- Assets/script/SpriteControl.cs
+++ Assets/script/SpriteControl.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 
 public class SpriteControl : MonoBehaviour {
+
 	Animator anim;
+    int speed;
+
 	// Use this for initialization
 	void Awake () {
 		anim = GetComponentInChildren<Animator>();
+        speed = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Vector3 moveDir = Vector3.zero;
+
 		int inputHor;
 		if(Input.GetKey(KeyCode.LeftArrow))
 			inputHor = -1;
@@ -21,8 +27,14 @@ public class SpriteControl : MonoBehaviour {
 		if(inputHor!=0){
 			anim.SetInteger("dirc",inputHor/Mathf.Abs(inputHor));
 			anim.SetBool("moving",true);
+            if (inputHor == -1)
+                moveDir += Vector3.left;
+            else if (inputHor == 1)
+                moveDir += Vector3.right;
 		}else{
 			anim.SetBool("moving",false);
 		}
+
+        transform.position += (moveDir * Time.deltaTime * speed);
 	}
 }
