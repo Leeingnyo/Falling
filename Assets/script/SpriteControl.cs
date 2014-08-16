7e41bae1677c5528018b4329f7190086306b8a69
sparkly9399 <sparkly9399@kaist.ac.kr>
2014-08-16 12:17:40 +0900

    Add background hierarchy
    back
    mid
    fore

--- Assets/script/SpriteControl.cs
+++ Assets/script/SpriteControl.cs
@@ -18,12 +18,16 @@ public class SpriteControl : MonoBehaviour {
         Vector3 moveDir = Vector3.zero;
 
 		int inputHor;
-		if(Input.GetKey(KeyCode.LeftArrow))
-			inputHor = -1;
-		else if(Input.GetKey(KeyCode.RightArrow))
-			inputHor = 1;
-		else
-			inputHor = 0;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            inputHor = -1;
+        else if (Input.GetKey(KeyCode.RightArrow))
+            inputHor = 1;
+        else if (Input.GetKey(KeyCode.UpArrow))
+            inputHor = 2;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            inputHor = -2;
+        else
+            inputHor = 0;
 
 		if(inputHor!=0){
 			anim.SetInteger("dirc",inputHor/Mathf.Abs(inputHor));
@@ -32,29 +36,16 @@ public class SpriteControl : MonoBehaviour {
                 moveDir += Vector3.left;
             else if (inputHor == 1)
                 moveDir += Vector3.right;
+            else if (inputHor == 2)
+                moveDir += Vector3.up;
+            else if (inputHor == -2)
+                moveDir += Vector3.down;
 		}else{
 			anim.SetBool("moving",false);
 		}
 
-        if (Input.GetKey(KeyCode.Space))
-            if (Physics2D.Linecast()&& jump == false) { //grounded 도 추가
-                anim.SetBool("inAir", true);
-                jump = true;
-            }
-
         transform.position += (moveDir * Time.deltaTime * speed);
 	}
 
-    void FixedUpdate() {
-        if (jump) {
-            rigidbody2D.AddForce(new Vector2(0f, jumpForce));
-        }
-        jump = false;
-    }
-
-    void OnCollisionStay2D(Collision2D coll) {
-         
-        anim.SetBool("inAir", false);
-    }
 
 }
