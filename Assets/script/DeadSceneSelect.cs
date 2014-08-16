7f5ae63ad6e871a78b7fec9640b3676dd6ab0ecc
yulcat <alszmtm@naver.com>
2014-08-17 02:30:37 +0900

    Death scenes available

--- Assets/script/DeadSceneSelect.cs
+++ Assets/script/DeadSceneSelect.cs
@@ -1,17 +1,14 @@
 using UnityEngine;
 using System.Collections;
 public class DeadSceneSelect:MonoBehaviour{
-//	public float[] ScoreSplit;
-//	public GameObject[] DeadScenes;
-//	private GameObject sprite;
-//	void Awake(){
-////		transform.
-//	}
-//	void kill(float score){
-//		for(int i = 0; i < DeadScenes.Length; i++){
-//			if(score < ScoreSplit[i]){
-//
-//			}
-//	}
+	public GameObject[] deathScenes;
+	private GameObject sprite;
+	void Awake(){
+		sprite = transform.GetChild(0).gameObject;
+	}
+	void kill(int num){
+		sprite.SetActive(false);
+		deathScenes[num].SetActive(true);
+	}
 }
 
