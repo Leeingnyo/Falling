c104359fdf40af71a7605818989ad687cfe7d1b9
sparkly9399 <sparkly9399@kaist.ac.kr>
2014-08-16 17:50:54 +0900

    Complete Wood Spawner
    Should respawn wood in falling state

--- Assets/script/ScrollingScript.cs
+++ Assets/script/ScrollingScript.cs
@@ -6,17 +6,8 @@ using UnityEngine;
 /// </summary>
 public class ScrollingScript : MonoBehaviour
 {
-
     public bool isLooping = true;
-
     private List<Transform> backgroundPart;
-    private List<GameObject> woodList;
-    private List<Vector3> woodPosList;
-
-    public GameObject woodPrefab;
-    public float numWoods = 10;
-
-    private Vector3 recent
 
     void Start()
     {
@@ -24,7 +15,6 @@ public class ScrollingScript : MonoBehaviour
         if (isLooping)
         {
             backgroundPart = new List<Transform>();
-            woodList = new List<GameObject>();
             for (int i = 0; i < transform.childCount; i++)
             {
                 Transform child = transform.GetChild(i);
@@ -38,22 +28,6 @@ public class ScrollingScript : MonoBehaviour
             backgroundPart = backgroundPart.OrderBy(
               t => t.position.y
             ).ToList();
-        }
-        GameObject newParent = GameObject.Find("1-Background");
-        Transform firstChild = backgroundPart.FirstOrDefault();
-        Vector3 firstPosition = firstChild.transform.position;
-        Vector3 firstSize = (firstChild.renderer.bounds.max - firstChild.renderer.bounds.min);
-        Vector3 woodSize = (woodPrefab.renderer.bounds.max - woodPrefab.renderer.bounds.min);
-        int dy = (int)(Camera.main.transform.position.y/2 / numWoods);
-        int sum_y = 0;
-        for (int i = 0; i < numWoods; i++)
-        {
-            Vector3 newPos = new Vector3(Random.Range(firstPosition.x - firstSize.x / 2 + woodSize.x, firstPosition.x + firstSize.x / 2 - woodSize.x),
-                Random.Range(Camera.main.transform.position.y + sum_y, Camera.main.transform.position.y + sum_y), 0);
-            GameObject wood = Instantiate(woodPrefab, newPos, Quaternion.identity) as GameObject;
-            sum_y += dy;
-            wood.transform.parent = newParent.transform;
-            woodList.Add(wood);
         }
     }
 
@@ -73,10 +47,6 @@ public class ScrollingScript : MonoBehaviour
                     backgroundPart.Remove(firstChild);
                     backgroundPart.Add(firstChild);                    
                 }
-            }
-            if (woodPosList < Camera.main.transform.position.y)
-            {
-
             }
         }
     }
---
+++ Assets/script/WoodSpawner.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class WoodSpawner : MonoBehaviour {
+    private List<GameObject> woodList;
+    public List<Vector3> woodPosList;
+    public GameObject woodprefab;
+    public GameObject bgprefab;
+    public int max_wood_cnt;
+    public int dy;
+
+    private float x_min_bound;
+    private float x_max_bound;
+
+    GameObject newParent;
+
+    void Awake()
+    {
+        woodList = new List<GameObject>();
+        woodPosList = new List<Vector3>();
+        x_min_bound = bgprefab.renderer.bounds.min.x + (woodprefab.renderer.bounds.max.x - woodprefab.renderer.bounds.min.x);
+        x_max_bound = bgprefab.renderer.bounds.max.x - (woodprefab.renderer.bounds.max.x - woodprefab.renderer.bounds.min.x);
+    }
+
+	// Use this for initialization
+	void Start () {
+        int sum_y = dy;
+        
+        GameObject newParent = GameObject.Find("2-Midground");
+        for (int i = 0; i < max_wood_cnt; i++)
+        {
+            Vector3 newPos = new Vector3(Random.Range(x_min_bound, x_max_bound), Random.Range(sum_y - dy, sum_y), 0);
+            GameObject wood = Instantiate(woodprefab, newPos, Quaternion.identity) as GameObject;
+            sum_y += dy;
+            woodList.Add(wood);
+            woodPosList.Add(newPos);
+            wood.transform.parent = newParent.transform;
+        }
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.Find("Player");
+        for (int i = 0; i < max_wood_cnt; i++)
+        {
+            if (player.transform.position.y - 5 > woodList[i].transform.position.y)
+            {
+                int index = i;
+                if (index == 0)
+                    index = max_wood_cnt;
+                float last_y = woodList[index - 1].transform.position.y;
+                Vector3 newPos = new Vector3(Random.Range(x_min_bound, x_max_bound), Random.Range(last_y+5, last_y + dy +5), 0);
+                woodPosList.Add(newPos);
+                woodList[i].transform.position = newPos;
+            }
+        }
+	}
+}
+
