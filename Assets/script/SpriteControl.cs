7a425b0c8045916c79499ccf958c425fc2dc660c
Leeingnyo <dyd1928@naver.com>
2014-08-16 11:23:11 +0900

    grounded를 구현했지만 이상하게 되고 있음
    왜인지 확인해봐야함

--- Assets/script/SpriteControl.cs
+++ Assets/script/SpriteControl.cs
@@ -4,19 +4,29 @@ using System.Collections;
 public class SpriteControl : MonoBehaviour {
 
 	Animator anim;
-    int speed = 2;
-    float jumpForce = 100.0f;
+    public int speed = 2;
+    public float jumpForce = 500.0f;
+
     bool jump = false;
+    bool grounded = false;
+
+    Transform groundCheck;
 
 	// Use this for initialization
 	void Awake () {
 		anim = GetComponentInChildren<Animator>();
+        groundCheck = transform.Find("GroundCheck");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        //init
         Vector3 moveDir = Vector3.zero;
 
+        //check the grounded
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position);
+
+        //input the keys
 		int inputHor;
 		if(Input.GetKey(KeyCode.LeftArrow))
 			inputHor = -1;
@@ -25,23 +35,34 @@ public class SpriteControl : MonoBehaviour {
 		else
 			inputHor = 0;
 
-		if(inputHor!=0){
-			anim.SetInteger("dirc",inputHor/Mathf.Abs(inputHor));
-			anim.SetBool("moving",true);
+        int inputJump;
+        if (Input.GetKey(KeyCode.Space))
+            inputJump = 1;
+        else
+            inputJump = 0;
+
+        //reflect the keys
+        if (inputHor != 0)
+        {
+            anim.SetInteger("dirc", inputHor / Mathf.Abs(inputHor));
+            anim.SetBool("moving", true);
             if (inputHor == -1)
                 moveDir += Vector3.left;
             else if (inputHor == 1)
                 moveDir += Vector3.right;
-		}else{
-			anim.SetBool("moving",false);
-		}
+        }
+        else
+        {
+            anim.SetBool("moving", false);
+        }
 
-        if (Input.GetKey(KeyCode.Space))
-            if (jump == false) { //grounded 도 추가
+        if (inputJump != 0)
+            if (jump == false && grounded == true) {
                 anim.SetBool("inAir", true);
                 jump = true;
             }
 
+        //move
         transform.position += (moveDir * Time.deltaTime * speed);
 	}
 
@@ -50,6 +71,7 @@ public class SpriteControl : MonoBehaviour {
             rigidbody2D.AddForce(new Vector2(0f, jumpForce));
         }
         jump = false;
+        grounded = false;
     }
 
     void OnCollisionStay2D(Collision2D coll) {
